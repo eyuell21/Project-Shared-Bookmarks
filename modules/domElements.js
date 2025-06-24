08a87723ad9bdf07b78264815b14a7369eccf3d7
eyuell21 <abrahameyuel4@gmail.com>
2025-06-24 12:14:23 +0100

    Created a dom reference file.

---
+++ modules/domElements.js
@@ -0,0 +1,9 @@
+// domElements.js
+
+export const userSelect = document.getElementById("user-select");
+export const bookmarkList = document.getElementById("bookmark-list");
+export const noBookmarksMsg = document.getElementById("no-bookmarks-message");
+export const bookmarkForm = document.getElementById("bookmark-form");
+export const urlInput = document.getElementById("url");
+export const titleInput = document.getElementById("title");
+export const descriptionInput = document.getElementById("description");
